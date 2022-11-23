4a5d077f58194e42e2f65b0c7b5bfcb1eb7cc646
idoctorvic <loukianova@yahoo.com>
2022-11-23 16:18:32 +0300

    Task19 Palindrom

---
+++ Task19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+
+Console.Write("Enter a 5-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num % 10 == num / 10000) && (num / 10 % 10 == num /1000 % 10 ))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
